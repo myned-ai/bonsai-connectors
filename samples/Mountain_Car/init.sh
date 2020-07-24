7a9cb1263e2a66fb2ca6b51889a3cb73d4d1baa6
Antonios Makro <68746783+antoniosmakro@users.noreply.github.com>
2020-07-24 17:15:16 +0100

    Delete init.sh

--- samples/Mountain_Car/init.sh
+++
@@ -1,4 +0,0 @@
-export SIM_WORKSPACE=689d3bba-9eb4-4e29-b83f-6bde5c3ee2fa
-export SIM_ACCESS_KEY=MGYxODcwNTNkNmM2NGE5NDljMzYzOWFiMmVmOGJmMWE6YjllZDM3OTItZjRjZi00N2FiLWJjZGMtOWFlZTMzOWNjYjA3
-
-python3 bonsai3_gym_mountain_car.py
